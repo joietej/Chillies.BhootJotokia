9b472670e738a7a017c05d999504d8ac687872cb
Tejas Sahasrabudehe <tejas.sahasrabudhe@gmail.com>
2020-02-19 11:09:52 +0100

    Add project files.

---
+++ Chillies.BhootJotokia/Models/Light.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chillies.BhootJotokia.Models
+{
+    readonly struct Panel
+    {
+
+    }
+}
---
+++ Chillies.BhootJotokia/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Chillies.BhootJotokia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+    readonly struct Point
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Point(int x, int y) => (this.X, this.Y) = (x, y);
+
+    }
+}
