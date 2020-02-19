ce8d3100f94cc0e2ce5e1b4f54ac02dc918f247d
Tejas Sahasrabudehe <tejas.sahasrabudhe@gmail.com>
2020-02-19 14:13:54 +0100

    Updated unit tests

--- Chillies.BhootJotokia.Test/TestXmlProvider.cs
+++ Chillies.BhootJotokia.Test/TestXmlProvider.cs
@@ -1,6 +1,7 @@
 using Chillies.BhootJotokia.Core;
 using Chillies.BhootJotokia.Models;
 using FluentAssertions;
+using System;
 using System.IO;
 using Xunit;
 
@@ -9,7 +10,7 @@ namespace Chillies.BhootJotokia.Test
     public class TestXmlProvider
     {
         [Fact]
-        public async void LoadXmlAsAsync_Should_Deserialize_Xml_To_Object()
+        public async void LoadXmlAsAsync_Should_Deserialize_Xml_To_Object_WhenPathIsValid()
         {
             // Arrange
             var path = Path.GetFullPath($"../../../../Assets/BeerPack.xml");
@@ -17,8 +18,19 @@ namespace Chillies.BhootJotokia.Test
             // Act
             var actual = await new XmlProvider().LoadXmlAsAsync<Folding>(path);
 
-            //Assert
+            // Assert
             actual.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async void LoadXmlAsAsync_Should_ThrowException_WhenPathIsInvalid()
+        {
+            // Arrange
+            var path = Path.GetFullPath($"../../../../Assets/Foo.xml");
+
+            // Act & Assert
+            await Assert.ThrowsAsync<FileNotFoundException>(async () => await new XmlProvider().LoadXmlAsAsync<Folding>(path));
+
         }
     }
 }
