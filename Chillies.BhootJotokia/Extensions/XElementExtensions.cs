0b974be7eacfb1ad3af0d66c937ade3dd4a78536
Tejas Sahasrabudehe <tejas.sahasrabudhe@gmail.com>
2020-02-19 23:03:27 +0100

    fixed typo

--- Chillies.BhootJotokia/Extensions/XElementExtensions.cs
+++ Chillies.BhootJotokia/Extensions/XElementExtensions.cs
@@ -6,21 +6,21 @@ namespace Chillies.BhootJotokia.Extensions
 {
     public static class XElementExtensions
     {
-        public static string Atr(this XElement elemnt, string name) =>
-           elemnt.Attribute(XName.Get(name)).Value;
+        public static string Atr(this XElement element, string name) =>
+           element.Attribute(XName.Get(name)).Value;
 
-        public static float AtrAsFloat(this XElement elemnt, string name) =>
-            float.TryParse(elemnt.Attribute(XName.Get(name)).Value, NumberStyles.Float, CultureInfo.InvariantCulture, out var value)
+        public static float AtrAsFloat(this XElement element, string name) =>
+            float.TryParse(element.Attribute(XName.Get(name)).Value, NumberStyles.Float, CultureInfo.InvariantCulture, out var value)
             ? value
             : default;
 
-        public static int AtrAsInt(this XElement elemnt, string name) =>
-           int.TryParse(elemnt.Attribute(XName.Get(name)).Value, out var value)
+        public static int AtrAsInt(this XElement element, string name) =>
+           int.TryParse(element.Attribute(XName.Get(name)).Value, out var value)
            ? value
            : default;
 
-        public static Guid AtrAsGuid(this XElement elemnt, string name) =>
-           Guid.TryParse(elemnt.Attribute(XName.Get(name)).Value, out var value)
+        public static Guid AtrAsGuid(this XElement element, string name) =>
+           Guid.TryParse(element.Attribute(XName.Get(name)).Value, out var value)
            ? value
            : default;
     }
